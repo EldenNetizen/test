814067f2ccd6353df4d7f77961a7350829d778d9
GolangGangster <1194090367@qq.com>
2026-06-15 14:00:31 +0800

    test1

---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿//#include"server.h"
+#include"thread_pool.h"
+#include <iostream>
+
+int main()
+{
+	
+	std::cout << "thread id:" << std::this_thread::get_id();
+	boost::asio::io_context io_context;
+	ThreadPool pool;
+	//Server s(io_context);
+	
+
+	//for (size_t i = 0; i < 8; ++i) {
+		//pool.enqueue([&io_context]() { io_context.run(); });
+	//}
+
+	while (1);
+	return 0;
+	
+}
